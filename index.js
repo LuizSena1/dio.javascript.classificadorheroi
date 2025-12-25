fc5b9be13700f023676930cf875d9027ad22d9d8
LuizSena1 <luizs.sena7@gmail.com>
2025-12-25 14:48:33 -0300

    Codigo do desafio completo

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-console.log("hello world")
+const readline = require('readline');
+const {stdin: input, stdout: output} = require('node:process');
+const rl = readline.createInterface({
+     input,
+     output});
+let username = '';
+let XP = 0;
+let RANK;
+rl.question('Bem Vindo Aventureiro. Porfavor, me diga seu nome! ', (name) => {
+    username = name;
+        rl.question(`Certo. ${username}, agora, me diga quantos pontos de Experiencia você tem. `, (points) => {
+            XP = Number(points);
+            switch (true) {
+                case (XP < 1000):
+                    RANK = 'Ferro';
+                    break;
+
+                case (XP <= 2000):
+                    RANK = 'Bronze';
+                    break;
+
+                case (XP <= 5000):
+                    RANK = 'Prata';
+                    break;
+
+                case (XP <= 7000):
+                    RANK = 'Ouro';
+                    break;
+
+                case (XP <= 8000):
+                    RANK = 'Platina';
+                    break;
+
+                case (XP <= 9000):
+                    RANK = 'Ascendente';
+                    break;
+
+                case (XP <= 10000):
+                    RANK = 'Imortal';
+                    break;
+            
+                default:
+                    RANK = 'Radiante';
+                    break;
+            }
+            console.log(`Cadastro completo, A Guilda lhe da boas vindas, Aqui seu cracha da guilda, você e rank ${RANK}`)
+            rl.close();
+        });
+});
+
+
